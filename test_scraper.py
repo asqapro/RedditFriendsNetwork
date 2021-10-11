6456f958f8821784ae15d96a80d31a6325d9719a
Adam Whisnant <asqapro11@gmail.com>
2021-10-11 01:51:49 -0500

    Added tests for parsing submissions and subreddit. Added comment to track dataset. Renamed variables to remove unncessary suffix.

--- test_scraper.py
+++ test_scraper.py
@@ -2,57 +2,86 @@ import pytest
 import praw
 from scraper import reddit_scraper
 
-reddit = praw.Reddit("FriendsNetwork")
-single_user1_submission = reddit.submission("XXXXX")
-example_user = reddit.redditor('asqapro')
+#Current dataset:
+# 3 submissions
+#    r/FriendsNetwork Lounge (autocreated)
+#      No comments
+#    [User1]
+#      No comments
+#    [User1][Replies]
+#      2 comments
+#      User1 replied to User1 1 time
 
 @pytest.fixture
-def reddit_instance():
+def reddit():
     return praw.Reddit("FriendsNetwork")
 
 @pytest.fixture
-def scraper_instance():
+def scraper():
     return reddit_scraper()
 
 @pytest.fixture
-def submission_user1_no_replies(reddit_instance):
-    return reddit_instance.submission("q5jlig")
+def subreddit(reddit):
+    return reddit.subreddit("FriendsNetwork")
 
 @pytest.fixture
-def submission_user1_replies(reddit_instance):
-    return reddit_instance.submission("q5mqas")
+def submission_user1_no_replies(reddit):
+    return reddit.submission("q5jlig")
 
-def test_parse_subreddit():
-    pass
+@pytest.fixture
+def submission_user1_replies(reddit):
+    return reddit.submission("q5mqas")
 
-def test_scrape_submission(scraper_instance, submission_user1_no_replies):
-    scraper_instance.add_scraped_submission(submission_user1_no_replies)
-    assert submission_user1_no_replies.id in scraper_instance.scraped_submissions
+@pytest.fixture
+def redditor_user1(reddit):
+    return reddit.redditor("FriendsNetworkDev")
 
-def test_mark_submissions_parsed(scraper_instance, submission_user1_no_replies):
-    scraper_instance.add_scraped_submission(submission_user1_no_replies)
-    scraper_instance.parse_scraped_submissions()
-    assert scraper_instance.scraped_submissions[submission_user1_no_replies.id]["parsed"] is True
+@pytest.fixture
+def redditor_user2(reddit):
+    return reddit.redditor("asqapro")
 
-def test_avoid_rescrape_submission(scraper_instance, submission_user1_no_replies):
-    scraper_instance.add_scraped_submission(submission_user1_no_replies)
-    scraper_instance.scraped_submissions[submission_user1_no_replies.id]["parsed"] = True
-    scraper_instance.add_scraped_submission(submission_user1_no_replies)
-    assert scraper_instance.scraped_submissions[submission_user1_no_replies.id]["parsed"] is True
+def test_scrape_submission(scraper, submission_user1_no_replies):
+    scraper.add_scraped_submission(submission_user1_no_replies)
+    assert submission_user1_no_replies.id in scraper.scraped_submissions
 
-def test_avoid_reparse_submission(scraper_instance, submission_user1_replies):
-    scraper_instance.add_scraped_submission(submission_user1_replies)
-    scraper_instance.parse_scraped_submissions()
-    assert scraper_instance.replies_graph.get_edge_data("FriendsNetworkDev", "FriendsNetworkDev", key=0)["weight"] == 1
-    scraper_instance.parse_scraped_submissions()
-    assert scraper_instance.replies_graph.get_edge_data("FriendsNetworkDev", "FriendsNetworkDev", key=0)["weight"] == 1
+def test_parse_submission_with_replies(scraper, submission_user1_replies, redditor_user1):
+    scraper.parse_submission(submission_user1_replies)
+    assert scraper.replies_graph.number_of_nodes() == 1
+    assert scraper.replies_graph.number_of_edges() == 1
+    assert scraper.replies_graph.get_edge_data(redditor_user1.name, redditor_user1.name, key=0)["weight"] == 1
 
-def test_parse_submission_no_replies(scraper_instance, submission_user1_no_replies):
-    scraper_instance.parse_submission(submission_user1_no_replies)
-    assert scraper_instance.replies_graph.number_of_nodes() == 0
-    assert scraper_instance.replies_graph.number_of_edges() == 0
+def test_parse_submission_no_replies(scraper, submission_user1_no_replies):
+    scraper.parse_submission(submission_user1_no_replies)
+    assert scraper.replies_graph.number_of_nodes() == 0
+    assert scraper.replies_graph.number_of_edges() == 0
 
-def test_parse_user():    
+def test_mark_submissions_parsed(scraper, submission_user1_no_replies):
+    scraper.add_scraped_submission(submission_user1_no_replies)
+    scraper.parse_scraped_submissions()
+    assert scraper.scraped_submissions[submission_user1_no_replies.id]["parsed"] is True
+
+def test_avoid_rescrape_submission(scraper, submission_user1_no_replies):
+    scraper.add_scraped_submission(submission_user1_no_replies)
+    scraper.scraped_submissions[submission_user1_no_replies.id]["parsed"] = True
+    scraper.add_scraped_submission(submission_user1_no_replies)
+    assert scraper.scraped_submissions[submission_user1_no_replies.id]["parsed"] is True
+
+def test_avoid_reparse_submission(scraper, submission_user1_replies,redditor_user1):
+    scraper.add_scraped_submission(submission_user1_replies)
+    scraper.parse_scraped_submissions()
+    assert scraper.replies_graph.get_edge_data(redditor_user1.name, redditor_user1.name, key=0)["weight"] == 1
+    scraper.parse_scraped_submissions()
+    assert scraper.replies_graph.get_edge_data(redditor_user1.name, redditor_user1.name, key=0)["weight"] == 1
+
+def test_parse_subreddit(scraper, subreddit, redditor_user1):
+    for submission in subreddit.top("all"):
+        scraper.add_scraped_submission(submission)
+    scraper.parse_scraped_submissions()
+    assert scraper.replies_graph.number_of_nodes() == 1
+    assert scraper.replies_graph.number_of_edges() == 1
+    assert scraper.replies_graph.get_edge_data(redditor_user1.name, redditor_user1.name, key=0)["weight"] == 1
+
+def test_parse_user():
     pass
 
 def test_nodes():
